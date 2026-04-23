ce39c0aea74892935fd10351694e808491604d0b
Brian Yuan <brian4685380@gmail.com>
2026-04-23 13:15:51 +0800

    add nvim-tree and telescope

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,51 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.pack.add({
+  { src = "https://github.com/nvim-tree/nvim-tree.lua" },
+  { src = "https://github.com/nvim-tree/nvim-web-devicons" },
+})
+
+local function on_attach(bufnr)
+  local api = require("nvim-tree.api")
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  api.map.on_attach.default(bufnr)
+
+  vim.keymap.set("n", "t", api.node.open.tab, opts("Open: New Tab"))
+end
+
+require("nvim-tree").setup({
+  on_attach = on_attach,
+  view = {
+    width = 34,
+    side = "left",
+  },
+  renderer = {
+    group_empty = true,
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+  },
+  git = {
+    enable = true,
+    ignore = false,
+  },
+  update_focused_file = {
+    enable = true,
+    update_root = true,
+  },
+  actions = {
+    open_file = {
+      quit_on_open = false,
+    },
+  },
+})
+
+vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle nvim-tree" })
+vim.keymap.set("n", "<leader>o", "<cmd>NvimTreeFocus<CR>", { desc = "Focus nvim-tree" })
+vim.keymap.set("n", "<leader>fe", "<cmd>NvimTreeFindFile<CR>", { desc = "Find current file" })
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,22 @@
+vim.pack.add({
+  { src = "https://github.com/nvim-telescope/telescope.nvim" },
+  { src = "https://github.com/nvim-lua/plenary.nvim" },
+})
+
+local telescope = require("telescope")
+local actions = require("telescope.actions")
+
+telescope.setup({
+  defaults = {
+    mappings = {
+      i = {
+        ["<esc>"] = actions.close,
+      },
+    },
+  },
+})
+
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Find files" })
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Live grep" })
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Buffers" })
+vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Help tags" })
