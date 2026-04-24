ca7175499b983d09e0f945b9a51625c2db49000e
Brian Yuan <brian4685380@gmail.com>
2026-04-24 21:45:26 +0800

    add hover event

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -6,9 +6,14 @@ require("bufferline").setup({
   options = {
     diagnostics = "nvim_lsp",
     always_show_bufferline = true,
-    show_buffer_close_icons = false,
+    show_buffer_close_icons = true,
     show_close_icon = false,
     separator_style = "slant",
+    hover = {
+      enabled = true,
+      delay = 200,
+      reveal = { "close" },
+    },
   },
 })
 
