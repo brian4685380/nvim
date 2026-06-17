a2854b9ba3610241598deadbe101683addb6312a
Brian Yuan <brian4685380@gmail.com>
2026-06-17 23:40:46 +0800

    add lazygit

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -14,11 +14,36 @@ vim.o.gcr = "n-v-c-sm-i-t:block,ci-ve:ver25,r-cr-o:hor20"
 vim.o.undofile = true
 vim.o.path = vim.o.path .. "**"
 vim.o.wildmenu = true
+vim.o.updatetime = 500
 --globals
 vim.g.tex_flavor = "latex"
 vim.g.mapleader = " "
 vim.g.netrw_liststyle = 3
 vim.o.gcr = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50"
 
-vim.o.spell = true
 vim.o.spelllang = "en_us"
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "gitcommit", "markdown", "text" },
+  callback = function()
+    vim.opt_local.spell = true
+  end,
+})
+
+vim.api.nvim_create_autocmd("CursorHold", {
+  group = vim.api.nvim_create_augroup("diagnostic_float", { clear = true }),
+  callback = function()
+    vim.diagnostic.open_float(nil, {
+      scope = "cursor",
+      focusable = false,
+      border = "single",
+      source = true,
+      close_events = {
+        "BufLeave",
+        "CursorMoved",
+        "InsertEnter",
+        "FocusLost",
+      },
+    })
+  end,
+})
