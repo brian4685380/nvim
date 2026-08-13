b86cdb4275c876e13172332a726a84efbd4ed4ca
Brian Yuan <brian4685380@gmail.com>
2026-08-13 10:33:10 +0800

    update completion

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,5 +1,8 @@
 vim.pack.add({
-  { src = "https://github.com/saghen/blink.cmp", version = "1.*" },
+  {
+    src = "https://github.com/Saghen/blink.cmp",
+    version = vim.version.range("1.*"),
+  },
 })
 
 vim.pack.add({
